016eed2362493089047bdb98a6e7e2da05fc0e12
gordomium <x@gordomium.me>
2016-03-05 00:07:30 +0800

    Fix api error state

--- routers/apis.js
+++ routers/apis.js
@@ -10,7 +10,6 @@ router.use(function* (next) {
   try {
     yield next
   } catch (err) {
-    this.status = err.status || 500
     this.body = err.message
     this.app.emit('error', err, this)
   }
